c76969591755b8163b038147371f91de121bc0e8
austinhenneberg <austin.henneberg@ufl.edu>
2026-07-11 15:17:58 -0400

    Makes menu functional and performs tasks based on user input

--- Crime.h
+++ Crime.h
@@ -27,8 +27,28 @@ public:
         Record rcd(r,a,c);
         records.push_back(rcd);
     };
-    vector<Record> getRecords() {
+    const vector<Record>& getRecords() {
         return this->records;
+    }
+    vector<pair<int,int>> getAreas() {
+        vector<pair<int,int>> areas;
+        int s = this->records.size();
+        for (int i = 0; i < s; i++) {
+            Crime::Record rcd  = this->records[i];
+            pair<int,int> area = {rcd.recordID,rcd.areaID};
+            areas.push_back(area);
+        }
+        return areas;
+    }
+    vector<pair<int,int>> getCrimes() {
+        vector<pair<int,int>> crimes;
+        int s = this->records.size();
+        for (int i = 0; i < s; i++) {
+            Crime::Record rcd  = this->records[i];
+            pair<int,int> area = {rcd.recordID,rcd.crimeID};
+            crimes.push_back(area);
+        }
+        return crimes;
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -3,10 +3,88 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <chrono>
+#include <regex>
 #include "Crime.h"
+#include "HeapSort.h"
+#include "MergeSort.h"
+
 using namespace std;
 
+bool PrintMostOrLeast(vector<pair<int,int>> data, map<int, string> IDMap, bool hi, int amt) {
+    int s = data.size();
+    if (amt > s) {
+        cout << "Amount is too large, try a different value.";
+        return false;
+    }
+    //highest
+    if (hi == 1) {
+        for (int i = s-1; i >= s-amt; i--) {
+            cout << IDMap[data[i].first] << ": " << data[i].second << " times" << endl;
+        }
+    }
+    //lowest
+    if (hi == 0) {
+        for (int i = 0; i < amt; i++) {
+            cout << IDMap[data[i].first] << ": " << data[i].second << " times" << endl;
+        }
+    }
+    return true;
+}
+bool VerifyAndReportSort(vector<pair<int,int>> hSort, vector<pair<int,int>> mSort, int heapTime, int mergeTime) {
+    string notValidMsg = "Sorting methods are not valid.";
+    //verify both sort vectors are the same size before proceeding.
+    cout << "Heap Sort: " << hSort.size() << " records" << endl;
+    cout << "Merge Sort: " << mSort.size() << " records" << endl;
+
+    if (hSort.size() != mSort.size()) {
+        cout << notValidMsg << " Methods vary in size." << endl;
+        return false;
+    }
+    for (int i = 0; i < hSort.size(); i++) {
+        if (hSort[i].second != mSort[i].second) {
+            cout << notValidMsg << " Methods vary in sort." << endl;
+            return false;
+        }
+    }
+    //Methods resulted in the same record count and record sort
+    cout << "Both methods resulted in the same record count and values." << endl;
+    cout << "Execution time for Heap Sort: " << heapTime << " microseconds" << endl;
+    cout << "Execution time for Merge Sort:" << mergeTime << " microseconds" << endl;
+    string fastSort = "";
+    string slowSort = "";
+    float faster = 0.0;
+
+    if (heapTime < mergeTime) {
+        fastSort = "Heap Sort"; slowSort = "Merge Sort";
+        faster = mergeTime / heapTime;
+    }
+    else {
+        fastSort = "Merge Sort"; slowSort = "Heap Sort";
+        faster = heapTime / mergeTime;
+    }
+    cout << fastSort << " was " << faster << "% faster than" << slowSort << endl;
+    return true;
+}
+int SortAndMeasure(vector<pair<int,int>>& data, map<int, string> IDMap, bool heap) {
+    auto t1 = std::chrono::system_clock::now();
+    if (heap) {
+        heapSort(data);
+    }
+    else {
+        mergeSort(data,IDMap,false);
+    }
+    auto t2 = std::chrono::system_clock::now();
+    auto t3 = t2-t1;
+    return t3.count();
+}
+void PrintOptions(map<int,string> options) {
+    for (auto it = options.begin(); it != options.end(); ++it) {
+        cout << it->second << endl;
+    }
+}
 int main() {
+    regex quote_re("\""); // regex quote
     // file spec variables
     string fileStr = "../resources/Crime_Data_from_2020_to_2024.csv";
     string colStr;
@@ -23,15 +101,18 @@ int main() {
     map<int, string> crimeIDMap;
     //User option varables
     map<int,string> userOptions = {
-        {1, "1. Most Frequent Crimes"},
-        {2, "2. Least Frequent Crimes"},
-        {3, "3. Crime counts in the LAPD Area"},
-        {4, "4. Find top area for a crime"},
-        {5, "5. Merge vs. Heap Sort"},
-        {6, "6. Exit"}
+        {1, "1. Find Most Frequent Crimes"},
+        {2, "2. Find Least Frequent Crimes"},
+        {3, "3. Get all Crime counts in the LAPD Area"},
+        // {4, "4. Find top area for a crime"},
+        {4, "4. Find Areas with the most amount of crime."},
+        {5, "5. Find Areas with the least amount of crime."},
+        {6, "6. Merge vs. Heap Sort"},
+        {7, "7. Exit"}
     };
     // Welcome message
     cout << "Welcome to CrimeSort LA!" << endl;
+    cout << "Loading Records..." << flush;
     // Read file
     ifstream file(fileStr);
     if (!file.is_open()){
@@ -54,13 +135,13 @@ int main() {
                 area = stoi(colStr);
             }
             else if (currCol == 6) { // crime area description
-                areaDesc = colStr;
+                areaDesc = regex_replace(colStr, quote_re, "");
             }
             else if (currCol == 9) {
                 crime = stoi(colStr);
             }
             else if (currCol == 10) {
-                crimeDesc = colStr;
+                crimeDesc = regex_replace(colStr, quote_re, "");
             }
             // read rest of line and process end of line logic
             else if (currCol > 10) {
@@ -78,43 +159,129 @@ int main() {
             }
         }
     }
-    int userChoice = -1;
-    // user interaction screen
-    cout << linesNum << " Records Loaded." << endl << endl;
-    for (auto it = userOptions.begin(); it != userOptions.end(); ++it) {
-        cout << it->second << endl;
-    }
+    string userChoice = "";
+    int intChoice = -1;
+    cout << linesNum << " Records Loaded." << endl;
+    cout << "Sorting data and measuring performance..." << flush;
+
+    // Replicate data for sort comparison
+    vector<pair<int,int>> heapAreas = LA.getAreas();
+    vector<pair<int,int>> mergeAreas = LA.getAreas();
+    vector<pair<int,int>> heapCrime = LA.getCrimes();
+    vector<pair<int,int>> mergeCrime = LA.getCrimes();
+
+    // Get sorting times for each sorting method and dataset
+    int heapAreaT = SortAndMeasure(heapAreas,areaIDMap,true);
+    int mergeAreaT = SortAndMeasure(mergeAreas,areaIDMap,true);
+    int heapCrimeT = SortAndMeasure(heapCrime,areaIDMap,true);
+    int mergeCrimeT = SortAndMeasure(mergeCrime,areaIDMap,true);
+
+    //count by crime
+    auto crimeCount = countByCrime(LA.getRecords());
+    auto areaCount = countByArea(LA.getRecords());
+
+    //sort crime and areas
+    heapSort(crimeCount);
+    heapSort(areaCount);
+
+    cout << "Complete!" << endl;
     // get choice and determine if it is valid.
     bool choiceValid = false;
-    while (!choiceValid) {
-        cout << endl << "Enter Choice: ";
-        cin >> userChoice;
-        if (userOptions.find(userChoice) != userOptions.end()) {
-            cout << "Correct choice!" << endl;
-            choiceValid = true;
+    bool exit = false;
+    // user interaction screen
+    do {
+        cout << "Please choose one of the following:" << endl << endl;
+        PrintOptions(userOptions);
+        while (!choiceValid) {
+            cout << endl << "Enter Choice: ";
+            cin >> userChoice;
+            try {
+                intChoice = stoi(userChoice);
+                if (userOptions.find(intChoice) != userOptions.end()) {
+                    choiceValid = true;
+                }
+                else {
+                    cout << "Incorrect choice, try again." << endl;
+                }
+            }
+            catch (...) {
+                cout << "Incorrect choice, try again." << endl;
+            }
         }
-        else {
-            cout << "Incorrect choice, try again." << endl;
+        // Use choice to determine next step
+        if (intChoice == 1) {
+            //get most frequent crimes
+            int freq = 0;
+            cout << "How many crimes would you like to display?\t";
+            cin >> freq;
+            cout << "Top " << freq << " most frequent Crimes:" << endl;
+            PrintMostOrLeast(crimeCount, crimeIDMap, true, freq);
+            cout << "Press any key and enter to return to menu." << endl;
+            cin >> userChoice;
+        }
+        else if (intChoice == 2) {
+            //get least frequent crimes
+            int freq = 0;
+            cout << "How many crimes would you like to display?\t";
+            cin >> freq;
+            cout << "Top " << freq << " least frequent Crimes:" << endl;
+            PrintMostOrLeast(crimeCount, crimeIDMap, false, freq);
+            //reset input validation
+            choiceValid = false;
+            cout << "Press any key and enter to return to menu." << endl;
+            cin >> userChoice;
+        }
+        else if (intChoice == 3) {
+            //get Crime counts
+            cout << "All crime counts:" << endl;
+            PrintMostOrLeast(crimeCount, crimeIDMap, true, crimeCount.size());
+            //reset input validation
+            choiceValid = false;
+            cout << "Press any key and enter to return to menu." << endl;
+            cin >> userChoice;
+        }
+        else if (intChoice == 4) {
+            //print areas with most crime
+            int freq = 0;
+            cout << "How many areas would you like to display?\t";
+            cin >> freq;
+            cout << "Top " << freq << " Areas with the most amount of crime:" << endl;
+            PrintMostOrLeast(areaCount, areaIDMap, true, freq);
+            //reset input validation
+            choiceValid = false;
+            cout << "Press any key and enter to return to menu." << endl;
+            cin >> userChoice;
+        }
+        else if (intChoice == 5) {
+            //print areas with least crime
+            int freq = 0;
+            cout << "How many areas would you like to display?\t";
+            cin >> freq;
+            cout << "Top " << freq << " Areas with the least amount of crime:" << endl;
+            PrintMostOrLeast(areaCount, areaIDMap, false, freq);
+            //reset input validation
+            choiceValid = false;
+            cout << "Press any key and enter to return to menu." << endl;
+            cin >> userChoice;
+        }
+        else if (intChoice == 6) {
+            // Test and Verify Merge vs Heap Sort
+            cout << "Area Data:" <<endl;
+            cout << "Validating sort methods..." << endl;
+            VerifyAndReportSort(heapAreas,mergeAreas, heapAreaT, mergeAreaT);
+            cout << endl << "Crime Data:" <<endl;
+            cout << "Validating sort methods..." << endl;
+            VerifyAndReportSort(heapCrime, mergeCrime, heapCrimeT, mergeAreaT);
+            //reset input validation
+            choiceValid = false;
+            cout << "Press any key and enter to return to menu." << endl;
+            cin >> userChoice;
+        }
+        else if (intChoice == 7) {
+            cout << "Exiting application. Have a good one!" << endl;
+            exit = true;
         }
     }
-    // Use choice to determine next step
-    if (userChoice == 1) {
-        //get most frequent crimes
-    }
-    else if (userChoice == 2) {
-        //get least frequent crimes
-    }
-    else if (userChoice == 3) {
-        //get Crime counts
-    }
-    else if (userChoice == 4) {
-        //get area with most crime
-    }
-    else if (userChoice == 5) {
-        // Test and Verify Merge vs Heap Sort
-    }
-    else if (userChoice == 6) {
-        cout << "Have a good one!" << endl;
-    }
+    while (!exit);
     return 0;
 }
